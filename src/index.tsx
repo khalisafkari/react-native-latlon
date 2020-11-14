f365a489a758b452d633c5754387a9a19597efd5
Khalis Afkari <khalis411@gmail.com>
2020-11-14 17:11:12 +0700

    ff

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,12 @@
-import { NativeModules, Platform } from 'react-native';
-const { Latlon } = NativeModules;
+import { NativeModules } from 'react-native';
 export type results = {
   lon: number;
   lat: number;
   city: string;
   country: string;
 };
-
-const getCountry = (): Promise<results> => {
-  if (Platform.OS === 'android') {
-    return new Promise((resolve, reject) => {
-      Latlon.getIp().then(resolve).catch(reject);
-    });
-  } else {
-    return new Promise<results>((resolve, reject) => {
-      fetch('https://api-geolocation.zeit.sh')
-        .then((res) => res.json())
-        .then(resolve)
-        .catch(reject);
-    });
-  }
+type LatlonType = {
+  getIp(): Promise<results>;
 };
-
-export default getCountry();
+const { Latlon } = NativeModules;
+export default Latlon as LatlonType;
